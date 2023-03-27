5d929f20717ec681d354de1eadf46fcd13b9cae8
Ibrahim2750mi <cshore.2750mi@gmail.com>
2023-03-27 14:47:06 +0530

    Make more horror

--- src/views/aftermath.py
+++ src/views/aftermath.py
@@ -1,5 +1,8 @@
+import time
+
 import arcade
 import arcade.gui
+import pyglet
 
 import config
 
@@ -8,13 +11,15 @@ class Aftermath(arcade.View):
     def __init__(self, score, win, dies_irae_player=None):
         super().__init__()
 
+        self.win = win
+
         self.manager = arcade.gui.UIManager()
         self.anchor = self.manager.add(arcade.gui.UIAnchorLayout())
 
         self.v_box = arcade.gui.UIBoxLayout(space_between=20)
 
         win_loose_message = "You win!\nCongrats for escaping!!"
-        if not win:
+        if not self.win:
             arcade.set_background_color(arcade.color.EERIE_BLACK)
             win_loose_message = "You lose\nYOU ARE NOW TRAPPED"
 
@@ -42,15 +47,37 @@ class Aftermath(arcade.View):
             anchor_y="center_y",
         )
 
-        self.start_bg_music = arcade.load_sound(config.ASSET_PATH / "02.A-Creepyscape.ogg", streaming=False)
-        self.start_bg_player = None
+        self.start_bg_music: arcade.Sound = None
+        self.start_bg_player: pyglet.media.Player = None
+
+        self.player: pyglet.media.Player = None
 
     def on_show_view(self):
         self.manager.enable()
+
+        self.start_bg_music = arcade.load_sound(config.ASSET_PATH / "02.A-Creepyscape.ogg", streaming=False)
+
+        if not self.win:
+            self.player = pyglet.media.Player()
+            self.player.loop = True
+            self.player.queue(pyglet.media.load(str(config.ASSET_PATH / "rain.mp4")))
+            self.player.play()
 
     def on_hide_view(self):
         self.manager.disable()
 
     def on_draw(self):
         self.clear()
+
+        with self.window.ctx.pyglet_rendering():
+            self.window.ctx.disable(self.window.ctx.BLEND)
+            video_texture = self.player.texture
+            if video_texture:
+                video_texture.blit(
+                    0,
+                    0,
+                    width=self.window.width,
+                    height=self.window.height,
+                )
+
         self.manager.draw()
